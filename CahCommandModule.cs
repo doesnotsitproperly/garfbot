53f1b5afabe6661532f61922e0b65c95152e9663
DoesNotSitProperly <doesnotsitproperly@gmail.com>
2022-03-05 11:04:59 -0500

    Add CahSubmitCard and attempt to implement Card Czar

--- CahCommandModule.cs
+++ CahCommandModule.cs
@@ -11,6 +11,9 @@ using System.Threading.Tasks;
 public class CahCommandModule : BaseCommandModule {
     string cahDir = Path.Combine(Directory.GetCurrentDirectory(), "Cah");
 
+    string cardCzar;
+    List<Tuple<string, string>> submittedCards;
+
     [Command("cahstart")]
     public async Task CahStartCommand(CommandContext ctx, params DiscordMember[] names) {
         File.Copy(
@@ -29,7 +32,6 @@ public class CahCommandModule : BaseCommandModule {
                 whiteCards.Add(whiteCard);
             }
 
-            JsonObject player = new JsonObject();
             JsonArray cards = new JsonArray();
             for (int i = 0; i < 10; i++) {
                 int card = new Random().Next(0, whiteCards.Count);
@@ -37,6 +39,7 @@ public class CahCommandModule : BaseCommandModule {
                 whiteCards.RemoveAt(card);
             }
 
+            JsonObject player = new JsonObject();
             player.Add("cards", cards);
             player.Add("score", 0);
             players.Add(name.Id.ToString(), player);
@@ -73,5 +76,38 @@ public class CahCommandModule : BaseCommandModule {
         using (StreamWriter sw = new StreamWriter(Path.Combine(cahDir, "Players.json"))) {
             await sw.WriteAsync(playersString);
         }
+
+        submittedCards = new List<Tuple<string, string>>();
+
+        string playersText = File.ReadAllText(Path.Combine(cahDir, "Players.json"));
+        JsonNode playersNode = JsonNode.Parse(playersText);
+        JsonArray playersArray = playersNode["players"].AsArray();
+        cardCzar = (string)playersArray[new Random().Next(playersArray.Count + 1)];
+    }
+
+    [Command("cahsubmitcard")]
+    public async Task CahSubmitCard(CommandContext ctx, int card) {
+        string id = ctx.Message.Id.ToString();
+
+        string jsonText = File.ReadAllText(Path.Combine(cahDir, "Players.json"));
+        JsonNode jsonNode = JsonNode.Parse(jsonText);
+
+        if (!jsonNode["players"].AsArray().Contains(id)) {
+            await ctx.RespondAsync("cannot find you in the list of players :/");
+            return;
+        }
+
+        if (cardCzar == id) {
+            return;
+        }
+
+        foreach (Tuple<string, string> submittedCard in submittedCards) {
+            if (submittedCard.Item2 == id) {
+                return;
+            }
+        }
+
+        JsonArray cardArray = jsonNode["players"][id]["cards"].AsArray();
+        submittedCards.Add(new Tuple<string, string>((string)cardArray[card], id));
     }
 }
