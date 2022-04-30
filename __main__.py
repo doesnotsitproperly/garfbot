1a4f32029b5a4d20a65ed8a0f612361cf892b91f
doesnotsitproperly <doesnotsitproperly@gmail.com>
2022-04-30 13:26:26 -0400

    Implement beginnings of CAH

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import nextcord, os, random, sys
+import json, nextcord, os, random, sys
 from nextcord.ext import commands
 from youtube_dl import YoutubeDL
 
 from garf_data import GarfData
-from util import *
+from util import inclusive_range, index_len, int_from_str, jsonc_loads
 
 current_dir = os.getcwd()
 cah_dir = os.path.join(current_dir, "cards_against_humanity")
@@ -26,7 +26,7 @@ async def on_ready():
 async def on_message(ctx: context):
     msg = ctx.content.lower()
 
-    if ctx.author.id is bot.user.id:
+    if ctx.author.id == bot.user.id:
         return
 
     # React with :eyes: if someone mentions lasagna
@@ -120,19 +120,6 @@ async def roll(ctx: context, number: str, size: str):
 
     await ctx.reply(final_message)
 
-# @bot.command()
-# async def start_game(ctx: Context, *players: nextcord.User):
-#     with open(os.path.join(cah_dir, "cards.json"), "r") as f:
-#         cards_dict = json.loads(f.read())
-# 
-#     game_file = os.path.join(cah_dir, "game.json")
-# 
-#     game_dict = {
-#         "players": []
-#     }
-#     for player in players:
-#         pass
-
 # Join voice
 @bot.command()
 async def join(ctx: context):
@@ -163,7 +150,7 @@ async def play(ctx: context, link: str):
         await ctx.reply("i'm not in a voice channel!")
         return
 
-    if data.path_to_ffmpeg is "":
+    if data.path_to_ffmpeg == "":
         await ctx.reply("you need to set my `path_to_ffmpeg` to use `play`")
         return
 
@@ -183,5 +170,64 @@ async def play(ctx: context, link: str):
     player = nextcord.FFmpegPCMAudio(file, **{ "options": "-vn" }, executable = data.path_to_ffmpeg)
 
     ctx.voice_client.play(player, after = lambda e: print(f'player error: {e}') if e else None)
+
+# Start a game of Cards Against Humanity (WIP)
+@bot.command()
+async def start_game(ctx: context, chance: int = 15, *players: nextcord.User):
+    """Start a new Cards Against Humanity game with (chance)% chance of a card being fill-in-the-blank (WIP)"""
+
+    chance: float = chance / 100
+    if chance < 0:
+        chance = 0
+    elif chance > 1:
+        chance = 1
+
+    await ctx.channel.send("Cards Against Humanity is made by Cards Against Humanity LLC; learn more at https://www.cardsagainsthumanity.com/")
+
+    with open(os.path.join(cah_dir, "cards.jsonc"), "r") as f:
+        cards_dict = jsonc_loads(f.read())
+
+    game_file = os.path.join(cah_dir, "game.json")
+    if os.path.exists(game_file):
+        os.remove(game_file)
+
+    game_dict = {
+        "players": [],
+        "black_cards_in_play": cards_dict["black_cards"],
+        "white_cards_in_play": cards_dict["white_cards"]
+    }
+    for player in players:
+        player_dict = {
+            "id": player.id,
+            "cards": []
+        }
+        for _ in inclusive_range(1, 10):
+            random.seed()
+            if random.random() < chance:
+                white_card = "(FILLABLE)"
+            else:
+                random.seed()
+                white_card = game_dict["white_cards_in_play"][random.randint(0, index_len(game_dict["white_cards_in_play"]))]
+                game_dict["white_cards_in_play"].remove(white_card)
+            player_dict["cards"].append(white_card)
+        game_dict["players"].append(player_dict)
+
+        player_message = "```"
+        for i in inclusive_range(0, index_len(player_dict["cards"])):
+            card = player_dict["cards"][i]
+            player_message += f"{i} {card}\n"
+        player_message += "```"
+        await player.send(player_message)
+
+    with open(game_file, "w") as f:
+        f.write(json.dumps(game_dict, indent = 4) + "\n")
+
+    await ctx.reply("this feature isn't fully implemented yet")
+
+    # random.seed() ; czar = bot.get_user(game_dict["players"][random.randint(0, index_len(game_dict["players"]))]["id"])
+    # random.seed() ; black_card = game_dict["black_cards_in_play"][random.randint(0, index_len(game_dict["black_cards_in_play"]))]
+    # game_dict["black_cards_in_play"].remove(black_card)
+
+    # await ctx.channel.send(f"the Card Czar is {czar.mention}, the black card is {black_card}")
 
 bot.run(data.token)
